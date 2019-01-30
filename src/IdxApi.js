143463c0177673a12a6270783675fc928ccdeaac
Dmitriy Balashov <mockedcat@gmail.com>
2019-01-30 16:46:59 +0300

    persons searchPersonByImage, createPerson methods were added

--- src/IdxApi.js
+++ src/IdxApi.js
@@ -1,36 +1,37 @@
-import Users from './users/index.js'
-import Auth from './auth/index.js'
-import Records from './records/index.js'
-import Notifications from './notifications/index.js'
-import Sources from './Sources/index.js'
+import Users from "./users/index.js";
+import Auth from "./auth/index.js";
+import Records from "./records/index.js";
+import Notifications from "./notifications/index.js";
+import Sources from "./sources/index.js";
+import Persons from "./persons";
 
 export default class IDXApi {
-    constructor({token = null, endpoint = null}) {
-        this.endpoint = endpoint;
+  constructor({ token = null, endpoint = null }) {
+    this.endpoint = endpoint;
 
-        this.auth = new Auth({token, endpoint});
-        this.users = new Users({token, endpoint});
-        this.records = new Records({token, endpoint});
-        this.notifications = new Notifications({token, endpoint});
-        this.sources = new Sources({token, endpoint});
-    }
+    this.auth = new Auth({ token, endpoint });
+    this.users = new Users({ token, endpoint });
+    this.records = new Records({ token, endpoint });
+    this.notifications = new Notifications({ token, endpoint });
+    this.sources = new Sources({ token, endpoint });
+    this.persons = new Persons({ token, endpoint });
+  }
 
-    init(username, password) {
-        return this.auth.login(username, password).then((body) => {
-            const {token} = body;
-            this.setToken(token.key);
+  init(username, password) {
+    return this.auth.login(username, password).then(body => {
+      const { token } = body;
+      this.setToken(token.key);
 
-            return body
-        });
-    }
+      return body;
+    });
+  }
 
-    setToken(token){
-        this.auth.setToken(token);
-        this.users.setToken(token);
-        this.records.setToken(token);
-        this.notifications.setToken(token);
-        this.sources.setToken(token);
-    }
+  setToken(token) {
+    this.auth.setToken(token);
+    this.users.setToken(token);
+    this.records.setToken(token);
+    this.notifications.setToken(token);
+    this.sources.setToken(token);
+    this.persons.setToken(token);
+  }
 }
-
-
---
+++ src/persons/index.js
@@ -0,0 +1,33 @@
+import { post, headersAddBearerToken } from "../request";
+import { dataURItoBlob } from "../utils/helpers";
+import Api from "../Api";
+
+export default class Persons extends Api {
+  searchPersonByImage({ photo, ...restData }) {
+    const data = new FormData();
+
+    data.append("photo", dataURItoBlob(photo));
+
+    Object.keys(restData).forEach(key => {
+      data.append(key, restData[key]);
+    });
+
+    const headers = headersAddBearerToken(this.token);
+
+    return post(`${this.endpoint}/persons/search/`, data, headers);
+  }
+
+  createPerson({ photo, ...restData }) {
+    const data = new FormData();
+
+    data.append("photo", dataURItoBlob(photo));
+
+    Object.keys(restData).forEach(key => {
+      data.append(key, restData[key]);
+    });
+
+    const headers = headersAddBearerToken(this.token);
+
+    return post(`${this.endpoint}/persons/`, data, headers);
+  }
+}
