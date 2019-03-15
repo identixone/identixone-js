3a1c45d9a348703f1b22f881f166907128cb73cc
Dmitriy Balashov <mockedcat@gmail.com>
2019-03-15 17:37:14 +0300

    sources fields were updated

--- packages/identix-rest-api-js/src/idx-rest-api/features/sources/v1/index.js
+++ packages/identix-rest-api-js/src/idx-rest-api/features/sources/v1/index.js
@@ -7,36 +7,54 @@ const SOURCES = "sources/";
 export default class Sources extends Api {
   static getSorceData = ({
     name,
-    identify_facesize_threshold,
     identify_resolution_threshold,
     pps_timestamp,
+
     manual_create_facesize_threshold,
     manual_create_on_ha,
     manual_create_on_junk,
+    manual_check_liveness,
+    manual_create_liveness_only,
     manual_check_asm,
+
     auto_create_persons,
     auto_create_facesize_threshold,
+    auto_create_check_blur,
+    auto_create_check_exp,
     auto_create_on_ha,
     auto_create_on_junk,
     auto_check_face_angle,
-    auto_check_angle_treshold,
+    auto_check_angle_threshold,
+    auto_check_liveness,
+    auto_create_liveness_only,
     auto_check_asm,
+
+    store_images_for_confs,
   }) => ({
     name,
-    identify_facesize_threshold,
     identify_resolution_threshold,
     pps_timestamp,
+
     manual_create_facesize_threshold,
     manual_create_on_ha,
     manual_create_on_junk,
+    manual_check_liveness,
+    manual_create_liveness_only,
     manual_check_asm,
+
     auto_create_persons,
     auto_create_facesize_threshold,
+    auto_create_check_blur,
+    auto_create_check_exp,
     auto_create_on_ha,
     auto_create_on_junk,
     auto_check_face_angle,
-    auto_check_angle_treshold,
+    auto_check_angle_threshold,
+    auto_check_liveness,
+    auto_create_liveness_only,
     auto_check_asm,
+
+    store_images_for_confs,
   });
 
   getSources() {
