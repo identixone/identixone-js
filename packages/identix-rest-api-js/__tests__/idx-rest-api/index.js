14aa284f434fe30b9e7964ef7d9cb43794f56235
Dmitriy Balashov <mockedcat@gmail.com>
2020-04-02 12:12:57 +0300

    Notifications: transport field was removed

--- packages/identix-rest-api-js/__tests__/idx-rest-api/index.js
+++ packages/identix-rest-api-js/__tests__/idx-rest-api/index.js
@@ -163,7 +163,6 @@ describe("IdxApi test", () => {
     const SETTINGS_NOTIFICATIONS = "settings/notifications/";
     const mockedNotification = {
       is_active: false,
-      transport: 0,
       http_method: 0,
       destination_url: "https://mocked.com",
       conf_thresholds: [1],
